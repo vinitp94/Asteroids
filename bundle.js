b8d4495e64363ff50a61f8940fb256dcb61fc2b2
vinit_parikh <vinitp94@gmail.com>
2016-12-16 15:25:48 -0800

    Implement Game class

--- bundle.js
+++ bundle.js
@@ -46,11 +46,9 @@
 
 	document.write("it works");
 	
-	const MovingObject = __webpack_require__(1);
+	const Game = __webpack_require__(2);
 	
-	const mo = new MovingObject(
-	  { pos: [30, 30], vel: [10, 10], radius: 5, color: "#00FF00"}
-	);
+	window.Game = Game;
 
 
 /***/ },
@@ -64,11 +62,11 @@
 	  this.color = options.color;
 	}
 	
-	MovingObject.prototype.draw = function(canvas) {
-	  canvas.fillStyle = this.color;
-	  canvas.beginPath();
+	MovingObject.prototype.draw = function(ctx) {
+	  ctx.fillStyle = this.color;
+	  ctx.beginPath();
 	
-	  canvas.arc(
+	  ctx.arc(
 	    this.pos[0],
 	    this.pos[1],
 	    this.radius,
@@ -77,7 +75,7 @@
 	    false
 	  );
 	
-	  canvas.fill();
+	  ctx.fill();
 	};
 	
 	MovingObject.prototype.move = function() {
@@ -86,6 +84,85 @@
 	};
 	
 	module.exports = MovingObject;
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const Asteroid = __webpack_require__(3);
+	
+	function Game() {
+	  this.DIM_X = 1000;
+	  this.DIM_Y = 1000;
+	  this.NUM_ASTEROIDS = 25;
+	  this.asteroids = [];
+	  this.addAsteroids();
+	}
+	
+	Game.prototype.addAsteroids = function() {
+	  for(let i = 0; i < this.NUM_ASTEROIDS; i++) {
+	    this.asteroids.push(new Asteroid(this.randomPosition()));
+	  }
+	};
+	
+	Game.prototype.randomPosition = function() {
+	  return [Math.random() * this.DIM_X, Math.random() * this.DIM_Y];
+	};
+	
+	Game.prototype.draw = function(ctx) {
+	  ctx.clearRect();
+	  this.asteroids.forEach( asteroid => asteroid.draw(ctx) );
+	};
+	
+	Game.prototype.moveObjects = function() {
+	  this.asteroids.forEach( asteroid => asteroid.move() );
+	};
+	
+	module.exports = Game;
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const MovingObject = __webpack_require__(1);
+	const Util = __webpack_require__(4);
+	
+	function Asteroid(pos) {
+	  let options = { pos: pos, vel: Util.randomVec(1), radius: 2, color: "#00FF00"};
+	  MovingObject.call(this, options);
+	}
+	
+	Util.inherits(Asteroid, MovingObject);
+	
+	module.exports = Asteroid;
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	const Util = {
+	  inherits (child, parent) {
+	    function Surrogate() {}
+	    Surrogate.prototype = parent.prototype;
+	    child.prototype = new Surrogate();
+	    child.prototype.constructor = child;
+	  },
+	
+	  randomVec (length) {
+	    const deg = 2 * Math.PI * Math.random();
+	    return Util.scale([Math.sin(deg), Math.cos(deg)], length);
+	  },
+	  
+	  // Scale the length of a vector by the given amount.
+	  scale (vec, m) {
+	    return [vec[0] * m, vec[1] * m];
+	  }
+	};
+	
+	module.exports = Util;
 
 
 /***/ }
